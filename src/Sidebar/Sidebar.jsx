19ae217eb0f3d9ec91d950691cb8d9ce50ea6746
kumsa Mergia <kumsamergia@gmail.com>
2025-03-18 10:55:01 +0300

    Setup the project

---
+++ src/Sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import { assets } from "../assets/assets";
+import { assets } from "../assets/assets";
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* top section */}
+      <div>
+        <img src={assets.menu_icon} alt="" />
+      </div>
+      {/* bottom section */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Sidebar;
