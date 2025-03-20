608702c61c19a1f6df7d7b7242b69e5697b5cff9
kumsa Mergia <kumsamergia@gmail.com>
2025-03-20 15:25:32 +0300

    Sidebar Section

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,42 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets"; // Import the assets object
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+
   return (
-    <div className="p-4 min-h-screen w-64 bg-[#f0f4f9] flex flex-col justify-between">
+    <div
+      className={`p-4 min-h-screen  ${
+        extended ? "w-64" : "w-16"
+      } bg-[#f0f4f9] flex flex-col justify-between transition-all duration-300`}
+    >
       {/* Top section */}
       <div>
-        <button className="mb-4">{assets.menu_icon}</button>
+        <button onClick={() => setExtended((prev) => !prev)} className="mb-4">
+          {assets.menu_icon}
+        </button>
 
         <div className="bg-white p-2 rounded-lg flex items-center gap-2 shadow-md">
           {assets.plus_icon}
-          <p className="text-gray-700">New Chat</p>
+          {extended ? <p className="text-gray-700">New Chat</p> : null}
         </div>
-
-        <div className="mt-6">
-          <p className="text-gray-600 text-sm">Recent</p>
-          <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 p-2 rounded-md mt-2">
-            {assets.message_icon}
-            <p className="text-gray-800">Who is Kumsa ...</p>
+        {extended ? (
+          <div className="mt-6">
+            <p className="text-gray-600 text-sm">Recent</p>
+            <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 p-2 rounded-md mt-2">
+              {assets.message_icon}
+              <p className="text-gray-800">Who is Kumsa ...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
 
       {/* Bottom section */}
       <div className="space-y-3">
         <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 p-2 rounded-md">
           {assets.question_icon}
-          <p className="text-gray-700">Help</p>
+          {extended ? <p className="text-gray-700">Help</p> : null}
         </div>
 
         <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 p-2 rounded-md">
           {assets.history_icon}
-          <p className="text-gray-700">Activity</p>
+          {extended ? <p className="text-gray-700">Activity</p> : null}
         </div>
 
         <div className="flex items-center gap-2 cursor-pointer hover:bg-gray-200 p-2 rounded-md">
           {assets.setting_icon}
-          <p className="text-gray-700">Settings</p>
+          {extended ? <p className="text-gray-700">Settings</p> : null}
         </div>
       </div>
     </div>
