9f2369004596bd744e84f0c737a69d491beb3794
Kumsa Mergia <kumsamergia@gmail.com>
2025-03-25 09:40:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
-});
+  build: {
+    outDir: 'dist' // Vite's default output directory
+  }
+})
