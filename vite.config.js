292f8b3e3f0b379ab1419052656dd86b92bcc854
kumsa Mergia <kumsamergia@gmail.com>
2025-03-25 13:18:21 +0300

    configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/gemini',  // Set to the path where your app will be hosted, if necessary
   plugins: [
     tailwindcss(),
     react()
